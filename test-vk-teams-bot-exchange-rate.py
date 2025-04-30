72dfe122f23b561aff1324458845fe9695c0dfb1
shumeyko <shumeiko.gl@gmail.com>
2025-04-30 14:09:27 +0300

    Update test-vk-teams-bot-exchange-rate.py

--- test-vk-teams-bot-exchange-rate.py
+++ test-vk-teams-bot-exchange-rate.py
@@ -232,8 +232,8 @@ class CurrencyService:
 currency_service = CurrencyService()
 
 def run_scheduler():
-    # Ежедневный отчёт в 10:55 (можно изменить при необходимости)
-    schedule.every().day.at("11:15").do(currency_service.send_daily_report)
+    # Ежедневный отчёт в 08:00 (можно изменить при необходимости)
+    schedule.every().day.at("05:00").do(currency_service.send_daily_report)
     schedule.every(55).minutes.do(lambda: logger.info("Self-ping для поддержания активности"))
 
     while True:
